05758b3901d41cf32641f671aac776e4b831a139
l-jdavies <drljdavies@gmail.com>
2021-07-27 11:32:38 -1000

    Build webpage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,15 +3,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Pioneer',
+  tagline: 'Feature flag management application',
+  //url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/documentation/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/pioneer.ico',
-  organizationName: 'pioneer', // Usually your GitHub org/user name.
-  projectName: 'pioneer-io', // Usually your repo name.
+  organizationName: 'pioneer-io', // Usually your GitHub org/user name.
+  projectName: 'documentation', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'Pioneer Documentation',
