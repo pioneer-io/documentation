907aebd28655a23b572161c8699ee16f59e6cbe1
l-jdavies <drljdavies@gmail.com>
2021-07-27 10:25:05 -1000

    Initial commit

---
+++ docs/getStarted.md
@@ -0,0 +1,9 @@
+---
+id: intro
+title: Getting Started
+slug: /
+hide_table_of_contents: true
+---
+
+# Pioneer
+## A light-weight feature flag management tool
---
+++ docusaurus.config.js
@@ -0,0 +1,113 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/pioneer.ico',
+  organizationName: 'pioneer', // Usually your GitHub org/user name.
+  projectName: 'pioneer-io', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Pioneer Documentation',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/pioneer_color graphic.png',
+      },
+      items: [
+        {
+          label: 'Docs',
+          position: 'left',
+          items: [
+            {to: '/', label: 'Getting Started', position: 'left'},
+            {to: 'Utilising sdks', label: 'Utilising SDKs', position: 'left'}
+          ]
+        }
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Project',
+          items: [
+            {
+              label: 'Github',
+              href: 'https://github.com/pioneer-io' 
+            }
+          ]
+        }
+        /*
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+        */
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Pioneer-io.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: './docs',
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
